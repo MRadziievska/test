56deffa1fbd02c3577bea8cb336db4e2ba1efd4e
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-07-24 13:27:47 +0300

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,8 @@
+print("How much does it coast")
+price = float(input())
+discount = float(input("Which discount do you have?: "))
+
+print("your discount =", discount)
+print(price)
+final_price = price - discount
+print("your_final_price", final_price)
---
+++ new_file.py
@@ -0,0 +1,18 @@
+first_variable = 9
+second_variable = 3
+
+print(first_variable - second_variable)
+print(first_variable + second_variable)
+print(first_variable * second_variable)
+print(first_variable / second_variable)
+print()
+
+print(first_variable + first_variable * second_variable)
+
+result_1 = first_variable + first_variable * second_variable
+result_2 = (first_variable + first_variable) * second_variable
+print(result_1)
+print(result_2)
+
+float_1 = 999.99
+print(type(float_1), float_1)
