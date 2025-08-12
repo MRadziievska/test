7f0dbc1eb9d52ebf6a5f1beb8ace548a0413f7e5
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-12 12:46:05 +0300

    Add files via upload

--- lesson_3_HW.py
+++ lesson_3_HW.py
@@ -1,10 +1,31 @@
-#2
-print("Sides of rectangle: ")
-width = int(input())
-height = int(input())
+# #2
+# print("Sides of rectangle: ")
+# width = int(input())
+# height = int(input())
+#
+# answer = 2 * (width + height)
+# print("P = ", answer)
+# from new_file import second_variable
 
-answer = 2 * (width + height)
-print("P = ", answer)
+# #3
+# print("Numbers: ")
+# number = str(input())
+# verified_number = bool(number)
+# print(type(verified_number), verified_number)
+
+print("Enter 3 numbers that add up to 100: ")
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+answer_1 = first_number + second_number + third_number
+assert answer_1 == 100, "wrong answer"
+print("correct answer")
+
+
+
+
+
 
 
 
