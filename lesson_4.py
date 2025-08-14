23435f9b88d4faa17ad9a90e650c34057e4900e4
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-14 14:21:04 +0300

    Add files via upload

---
+++ lesson_4.py
@@ -0,0 +1,20 @@
+
+# var_1 =3
+# print(var_1)
+#
+# if var_1 > 5:
+#     print("var_1 more than 5")
+# else:
+#     print("var_1 less or equal 5")
+#
+# print("it's a new line")
+
+
+secret = input("Please enter a secret phrase: ")
+
+if secret == "python":
+    print("Welcome")
+else:
+    print("You are wrong")
+
+print("subscribe to channel")
