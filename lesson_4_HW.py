d6bdfa052ca2c040fddb1f27458da5aa827fa40a
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-15 11:41:29 +0300

    Add files via upload

---
+++ lesson_4_HW.py
@@ -0,0 +1,33 @@
+#1
+
+# question = int(input("How old are you?: "))
+#
+# if question < 18:
+#     print("You're too young.")
+# else:
+#     print("Welcome!")
+
+#2
+
+# temperature_outside = int(input("What's the temperature outside?: "))
+# temperature_comfort = int(input("What's the temperature comfort?: "))
+#
+# if temperature_outside > temperature_comfort:
+#     print("Turn on the air conditioner.")
+# else:
+#     print("You might be comfortable without air conditioning.")
+
+#3
+
+question_1 = input("Переведите слово: паук - ")
+question_2 = int(input("2+5= "))
+question_3 = input("Введите 4 четных числа через запятую: ")
+
+if question_1.lower() == "spider" and question_2 == 7 and question_3 == "2,4,6,8":
+    print("5 баллов")
+else:
+    print("0 баллов")
+
+
+
+
