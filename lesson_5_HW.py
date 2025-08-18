5e758f54b6b0f81be3c186752d9534cbc05324cc
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-18 16:53:44 +0300

    Add files via upload

---
+++ lesson_5_HW.py
@@ -0,0 +1,32 @@
+#1
+# class_marks = [11,10,11,10,8]
+# rating = sum(class_marks)/len(class_marks)
+# print(rating)
+
+#2
+element = input("Please, enter 4 words: ").split()
+print(element)
+
+del_item = int(input("Delete word: "))
+element.pop(del_item-1)
+print(element)
+
+del_item = int(input("Delete word: "))
+element.pop(del_item-1)
+print(element, element.append("Maryna"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
