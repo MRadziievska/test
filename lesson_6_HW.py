b2ca3e4ed635ca8e9d1b7f6d8206e321aea07833
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-19 17:18:27 +0300

    Add files via upload

---
+++ lesson_6_HW.py
@@ -0,0 +1,26 @@
+#1
+# list_numbers = [1, 2, 3, 4, 5]
+#
+# total = 0
+# for i in list_numbers:
+#     total += i
+#
+# print(total)
+
+#2
+# numbers = [-2, -1, 0, 1, 2]
+#
+# for num in numbers:
+#     if num > 0:
+#         print(num)
+
+#3
+numbers = [-2, -1, 0, 1, 2]
+
+squares = []
+for i in numbers:
+    squares.append(i ** 2)
+
+print(squares)
+
+
