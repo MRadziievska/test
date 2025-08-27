dc3995fbd4bfc2ca81c1b96c290d9ab690d42e7e
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-08-27 15:07:37 +0300

    Add files via upload

---
+++ lesson_7_HW.py
@@ -0,0 +1,23 @@
+#1
+# counter = 0
+# while counter <= 10:
+#     print(counter)
+#     counter = counter + 1
+
+#2
+# counter = 1
+# suma = 0
+# while counter <= 100:
+#     suma += counter
+#     counter += 1
+#
+# print(suma)
+
+#3
+counter = 10
+while counter >= 1:
+    print(counter)
+    counter = counter - 1
+print("start")
+
+
